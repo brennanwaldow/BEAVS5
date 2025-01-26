ee26996c7c7355e75c3c1cbc5226d75aa125ca71
Brennan Waldow <brennanwaldow@users.noreply.github.com>
2025-01-25 16:13:08 -0800

    BMP390 altimeter collection
    - Also updated indentation from VSCode standard to Arduino IDE standard

--- BEAVS5_Main/BEAVS5_Main.ino
+++ BEAVS5_Main/BEAVS5_Main.ino
@@ -7,20 +7,20 @@ Accelerometer: Adafruit BNO055 IMU
 */
 
 // -----   Libraries   -----
-
-#include <string.h>
-
-using namespace std;
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
 
 // Utility
 
 float feet_to_meters(float length) {     // length [feet]
-    return length / 3.28084; // [meters]
+  return length / 3.28084; // [meters]
 }
 
 float meters_to_feet(float length) {     // length [meters]
-    return length * 3.28084; // [feet]
+  return length * 3.28084; // [feet]
 }
 
 
@@ -31,6 +31,14 @@ String BEAVS_version = "5.0.0";
 float launch_altitude = 0; // [meters]
 float launch_altimeter = 1013.25; // [HPa]
 float target_apogee = feet_to_meters(10000.0); // [meters], AGL
+
+// BMP390
+#define SEALEVELPRESSURE_HPA (launch_altimeter)
+#define BMP_SCK 13   // vvv TEMPORARY PINS: Reassign for PCB layout
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+Adafruit_BMP3XX bmp;
 
 // PID constants
 float kp = 6;
@@ -55,25 +63,33 @@ int flight_phase = 0;
 
 // -----   Power-On Boot   -----
 void setup() {
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  bmp.begin_I2C();
+
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_16X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 // -----   Control Loop   -----
 void loop() {
-    switch(flight_phase) {
-        case 0:
-            preflight_loop();
-            break;
-        case 1:
-            ready_loop();
-            break;
-        case 2:
-            flight_loop();
-            break;
-        case 3:
-            coast_loop();
-            break;
-    }
+  switch(flight_phase) {
+    case 0:
+      preflight_loop();
+      break;
+    case 1:
+      ready_loop();
+      break;
+    case 2:
+      flight_loop();
+      break;
+    case 3:
+      coast_loop();
+      break;
+  }
 }
 
 
@@ -84,6 +100,8 @@ void preflight_loop() {
   delay(1000);
 
   if (millis() > 5000) arm();
+
+  collect_telemetry();
 }
 
 void ready_loop() {
@@ -91,47 +109,56 @@ void ready_loop() {
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
+
+  collect_telemetry();
 }
 
 void flight_loop() {
-
+  collect_telemetry();
 }
 
 void coast_loop() {
-
+  collect_telemetry();
 }
 
 
 
 // -----   Phase Changeovers   -----
 void arm() {
-    // SAFETY PIN REMOVED: Arm BEAVS monitoring and initiate startup
-    flight_phase = 1;
+  // SAFETY PIN REMOVED: Arm BEAVS monitoring and initiate startup
+  flight_phase = 1;
 }
 
 void disarm() {
-    // SAFETY PIN REINSERTED: Return to Disarmed state
-    flight_phase = 0;
+  // SAFETY PIN REINSERTED: Return to Disarmed state
+  flight_phase = 0;
 }
 
 void launch() {
-    // ENGINE IGNITION: Arm BEAVS monitoring for cutoff
-    flight_phase = 2;
+  // ENGINE IGNITION: Arm BEAVS monitoring for cutoff
+  flight_phase = 2;
 }
 
 void coast() {
-    // ENGINE CUTOFF: Deploy BEAVS
-    flight_phase = 3;
+  // ENGINE CUTOFF: Deploy BEAVS
+  flight_phase = 3;
 }
 
 void descend() {
-    // APOGEE REACHED: BEAVS safing, PID shutdown
-    flight_phase = 4;
+  // APOGEE REACHED: BEAVS safing, PID shutdown
+  flight_phase = 4;
 }
 
 
 
 // -----   Functions   -----
+
+void collect_telemetry() {
+  bmp.performReading();
+
+  float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  height = altitude - launch_altitude;
+}
 
 // Tick PID controller
 void PID() {
