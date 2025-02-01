2ab65d78a1fd1e165ace9fccd4c76e1a654eddb7
Brennan Waldow <brennanwaldow@users.noreply.github.com>
2025-02-01 15:26:00 -0800

    Servo driver
    - Changed BMP pins out for I2C setup (oops)
    - Enumified the Flight Phase
    - Added servo driver for DS3235

--- BEAVS5_Main/BEAVS5_Main.ino
+++ BEAVS5_Main/BEAVS5_Main.ino
@@ -4,6 +4,7 @@ Hardware:
 Computer: Raspberry Pi Pico RP2040
 Altimeter: Adafruit BMP390
 Accelerometer: Adafruit BNO055 IMU
+Servo: DS3235
 */
 
 // -----   Libraries   -----
@@ -34,16 +35,17 @@ float target_apogee = feet_to_meters(10000.0); // [meters], AGL
 
 // BMP390
 #define SEALEVELPRESSURE_HPA (launch_altimeter)
-#define BMP_SCK 13   // vvv TEMPORARY PINS: Reassign for PCB layout
-#define BMP_MISO 12
-#define BMP_MOSI 11
-#define BMP_CS 10
+#define BMP_SCK 2   // vvv TEMPORARY PINS: Reassign for PCB layout
+#define BMP_MOSI 1   // SDA pin
 Adafruit_BMP3XX bmp;
 
+// Pins
+int servo_pin = 28;
+
 // PID constants
-float kp = 6;
-float ki = 9;
-float kd = 0;
+float kp = 1;
+float ki = 1;
+float kd = 1;
 
 // Flight Computer
 float height = 0; // [meters]
@@ -58,7 +60,8 @@ long clock_time = 0; // [ms]
 -- 3: Coast Phase (BEAVing)
 -- 4: Descent Phase
 */
-int flight_phase = 0;
+enum {PREFLIGHT, ARMED, FLIGHT, COAST, DESCENT};
+int flight_phase = PREFLIGHT;
 
 
 // -----   Power-On Boot   -----
@@ -72,21 +75,27 @@ void setup() {
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_16X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  pinMode(servo_pin, OUTPUT);
+
+  // Ensure servo is stowed
+  command_deflection(-1);
 }
 
 // -----   Control Loop   -----
 void loop() {
+
   switch(flight_phase) {
-    case 0:
+    case PREFLIGHT:
       preflight_loop();
       break;
-    case 1:
+    case ARMED:
       ready_loop();
       break;
-    case 2:
+    case FLIGHT:
       flight_loop();
       break;
-    case 3:
+    case COAST:
       coast_loop();
       break;
   }
@@ -94,21 +103,21 @@ void loop() {
 
 
 void preflight_loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(500);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(1000);
 
-  if (millis() > 5000) arm();
+  if (millis() > 3000) arm();
 
   collect_telemetry();
 }
 
 void ready_loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  // delay(100);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(100);
 
   collect_telemetry();
 }
@@ -126,27 +135,35 @@ void coast_loop() {
 // -----   Phase Changeovers   -----
 void arm() {
   // SAFETY PIN REMOVED: Arm BEAVS monitoring and initiate startup
-  flight_phase = 1;
+  flight_phase = ARMED;
+
+  command_deflection(1);
+  delay(2000);
+  command_deflection(0.5);
+  delay(1000);
+  command_deflection(0);
+  delay(3000);
+  command_deflection(-1);
 }
 
 void disarm() {
   // SAFETY PIN REINSERTED: Return to Disarmed state
-  flight_phase = 0;
+  flight_phase = PREFLIGHT;
 }
 
 void launch() {
   // ENGINE IGNITION: Arm BEAVS monitoring for cutoff
-  flight_phase = 2;
+  flight_phase = FLIGHT;
 }
 
 void coast() {
   // ENGINE CUTOFF: Deploy BEAVS
-  flight_phase = 3;
+  flight_phase = COAST;
 }
 
 void descend() {
   // APOGEE REACHED: BEAVS safing, PID shutdown
-  flight_phase = 4;
+  flight_phase = DESCENT;
 }
 
 
@@ -165,8 +182,28 @@ void PID() {
 
 }
 
-void command_deflection(float deflection) {  // [ratio], 0 to 1
+void command_deflection(float deflection) {  // [ratio], 0 (flush) to 1 (full extend); or -1 for full retract (inside tube Inner Diameter)
+  int min = 50;
+  int max = 180;
 
+  int max_pulse = 2500;
+  int min_pulse = 500;
+  
+  float angle = 0;
+
+  // Remap angle
+  if (deflection > -1) angle = deflection * (max - min) + min;
+
+  int x = (max_pulse - min_pulse) * (angle / max) + min_pulse;
+
+  for (int i = 0; i <= 2; i++) {
+    // A pulse each 20ms
+    digitalWrite(servo_pin, HIGH);
+    delayMicroseconds(x);
+    digitalWrite(servo_pin, LOW);
+    delayMicroseconds(18550);
+    // Pulses duration: 500 - 0deg; 1500 - 90deg; 2500 - 180deg
+  }
 }
 
 // Velocity lookup table
