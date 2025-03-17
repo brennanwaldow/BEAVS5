1dc968dec3ae41f13fb7711e41c34df667778db5
Brennan Waldow <brennanwaldow@users.noreply.github.com>
2025-03-17 16:02:50 -0700

    Velocity lookup adjusted to target altitude
    + New OpenRocket export with modified thrust curve to coast to 3052m (10013ft) on burnout
    + Added time condition for apogee to prevent failure if openrocket decides to be Silly and start with a negative velocity (which it did) (thanks openrocket)
    + Readjust Altitude component of table as required to intercept target perfectly

--- Utilities/lookup_table.py
+++ Utilities/lookup_table.py
@@ -2,7 +2,8 @@ import csv
 from matplotlib import pyplot as plt
 import numpy as np
 
-dataset = '5.3.2'
+dataset = '5.3.2_Modified'
+target_apogee = 3048 # [m]
 
 # Altitude [m], velocity [m/s]
 table = [[], []]
@@ -15,18 +16,28 @@ with open('Utilities/Data/' + dataset + '_DataSet.csv', newline='') as csvfile:
         # Break first row
         if (time == '# Time (s)'): continue
 
+        time = float(time)
         altitude = float(row[1])
         velocity = float(row[2])
         acceleration = float(row[3])
         thrust = float(row[28])
 
         # Stop at apogee
-        if (velocity < 0): break
+        if (velocity < 0 and time > 0.5): break
 
         # Wait until motor cutout to build table
         if (thrust == 0):
             table[0].append(altitude)
             table[1].append(velocity)
+
+# Readjust altitude to finely intercept target
+apogee = table[0][-1]
+print(f'\nRaw apogee: {round(apogee, 4)} m')
+difference = target_apogee - apogee
+for i in range(len(table[0])):
+    table[0][i] = table[0][i] + difference
+apogee = table[0][-1]
+print(f'Adjusted apogee: {round(apogee, 2)} m')
 
 # Polyfit
 constants = np.polyfit(table[0], table[1], 10)
@@ -41,7 +52,7 @@ for const in constants:
     i += 1
 
 print('')
-print(f'Difference at apogee: {round(p(table[0][-1]), 3)} m/s\n')
+print(f'Polynomial deviation at apogee: {round(p(table[0][-1]), 3)} m/s\n')
 
 
 # Graph velocity table
