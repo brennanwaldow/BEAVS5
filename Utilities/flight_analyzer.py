48cf10fc23bbe80c92420ded475fa1428b2bc824
brennanwaldow <brennanwaldow@users.noreply.github.com>
2025-05-20 00:15:25 -0700

    Expand Flight Analyzer into subplots
    + Subplots for altitude, velocity/acceleration, and deployment angle/flight phase
    - Target apogee tick marks only appear if rocket passes 500m AGL to allow for easy analysis of ground data too
    
    + Ditch first two rows of data as altimeter/velocity struggle to catch up, gets rid of nasty spikes

--- Utilities/flight_analyzer.py
+++ Utilities/flight_analyzer.py
@@ -11,15 +11,18 @@ phase_changes = []
 target_agl = 3048
 launch_elevation = 1380
 
+skip = 0
+apogee = 0
+
 # Reader designed for BEAVS5 SD card logging
 with open('Utilities/Data/Flight Data/data_' + index + '.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        time = row[0]
-        # Break first row
-        if (time == '# Time (s)'): continue
+        # Skip first two data rows to avoid dodgy data as it calibrates
+        skip += 1
+        if (skip < 3): continue
 
-        time = float(time)
+        time = float(row[0])
         altitude = float(row[1])
         velocity = float(row[3])
         acceleration = float(row[4])
@@ -34,6 +37,8 @@ with open('Utilities/Data/Flight Data/data_' + index + '.csv', newline='') as cs
 
         graph[4].append(commanded_angle)
         graph[5].append(flight_phase)
+
+        if apogee < altitude: apogee = altitude
         
         if (flight_phase != current_phase):
             phase_changes.append(time)
@@ -41,29 +46,41 @@ with open('Utilities/Data/Flight Data/data_' + index + '.csv', newline='') as cs
 
 
 # Graph table
-fig, axes = plt.subplots(1, 1)
+fig, axes = plt.subplots(3, 1)
 
-ax2 = axes.twinx()
+# ax2 = axes.twinx()
 
-axes.plot(graph[0], graph[1], label='Altitude', color='r')
-axes.plot(graph[0], graph[2], label='Velocity', color='coral')
-axes.plot(graph[0], graph[3], label='Acceleration', color='goldenrod')
+axes[0].plot(graph[0], graph[1], label='Altitude', color='r')
+axes[1].plot(graph[0], graph[2], label='Velocity', color='coral')
+axes[1].plot(graph[0], graph[3], label='Acceleration', color='goldenrod')
 
-ax2.plot(graph[0], graph[4], label='Commanded Angle', color='darkmagenta')
-# axes.plot(graph[0], graph[5], label='Flight Phase', color='lightsalmon')
+axes[2].plot(graph[0], graph[4], label='Commanded Angle', color='darkmagenta')
+axes[2].plot(graph[0], graph[5], label='Flight Phase', color='lightsalmon')
 
-axes.axhline(y=target_agl + launch_elevation, color='black', ls='--')
+if apogee > 500: axes[0].axhline(y=target_agl + launch_elevation, color='black', ls='--')
 
 for phase_change in phase_changes:
-    plt.axvline(x=phase_change, color='black', ls='--')
+    axes[0].axvline(x=phase_change, color='black', ls='--')
+    axes[1].axvline(x=phase_change, color='black', ls='--')
+    axes[2].axvline(x=phase_change, color='black', ls='--')
 
-axes.set_xlabel('Time (s)')
-axes.set_ylabel(' ')
-axes.grid(True)
-axes.legend()
+axes[0].set_xlabel('Time (s)')
+axes[0].set_ylabel(' ')
+axes[0].grid(True)
+axes[0].legend()
 
-ax2.set_ylabel("Commanded Angle (Degrees)", color='darkmagenta')
-ax2.tick_params(labelcolor='darkmagenta')
-ax2.set_ylim(0, 180)
+axes[1].set_xlabel('Time (s)')
+axes[1].set_ylabel(' ')
+axes[1].grid(True)
+axes[1].legend()
+
+axes[2].set_xlabel('Time (s)')
+axes[2].set_ylabel(' ')
+axes[2].grid(True)
+axes[2].legend()
+
+# ax2.set_ylabel("Commanded Angle (Degrees)", color='darkmagenta')
+# ax2.tick_params(labelcolor='darkmagenta')
+# ax2.set_ylim(0, 180)
 
 plt.show()
